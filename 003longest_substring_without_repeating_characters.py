c6fb66b2a779915be26d26af2c020403825f6dac
Owen Zhang <owen.zhang@emc.com>
2018-10-09 15:41:22 +0800

    resolve 003

---
+++ 003longest_substring_without_repeating_characters.py
@@ -0,0 +1,23 @@
+# Given a string, find the length of the longest substring without repeating characters.
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+
+        """
+        n = s.__len__()
+        myset = set()
+        l, start, end = 0,0,0
+        while(start < n and  end < n):
+            if s[end] not in myset:
+                myset.add(s[end])
+                end += 1
+                l = max(l, end-start)
+            else:
+                myset.remove(s[start])
+                start += 1
+        return l
+a = Solution()
+print(a.lengthOfLongestSubstring('dvdf'))
