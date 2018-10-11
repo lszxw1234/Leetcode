9da1bd4abfb97badbedc34a02349da54a0a16a8a
Owen Zhang <owen.zhang@emc.com>
2018-10-11 11:19:50 +0800

    resolve 004

--- 004median-of-two-sorted-arrays.py
+++ 004median-of-two-sorted-arrays.py
@@ -23,8 +23,43 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        a=sorted(nums1+nums2)
-        if len(a)%2==0:
-            return((a[int(len(a)/2.0)]+a[int(len(a)/2.0)-1])/2.0)
-        else:
-            return(a[int(len(a)/2.0)])
+        return self.median(nums1, nums2)
+
+    def median(self, A, B):
+        m, n = len(A), len(B)
+        if m > n:
+            A, B, m, n = B, A, n, m
+        if n == 0:
+            raise ValueError
+
+        imin, imax, half_len = 0, m, (m + n + 1) / 2
+        while imin <= imax:
+            i = (imin + imax) / 2
+            j = half_len - i
+            if i < m and B[j - 1] > A[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and A[i - 1] > B[j]:
+                # i is too big, must decrease it
+                imax = i - 1
+            else:
+                # i is perfect
+
+                if i == 0:
+                    max_of_left = B[j - 1]
+                elif j == 0:
+                    max_of_left = A[i - 1]
+                else:
+                    max_of_left = max(A[i - 1], B[j - 1])
+
+                if (m + n) % 2 == 1:
+                    return max_of_left
+
+                if i == m:
+                    min_of_right = B[j]
+                elif j == n:
+                    min_of_right = A[i]
+                else:
+                    min_of_right = min(A[i], B[j])
+
+                return (max_of_left + min_of_right) / 2.0
